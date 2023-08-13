b9db146d1f39d126a34fe93f8b78fdccaf36ec9a
Dragunov Semyon <neoxack@gmail.com>
2023-08-13 18:27:28 +0400

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# S3Kira.AspNetCore
+
+`Microsoft.Extensions.DependencyInjection` extension for S3Kira
+
+## 🔧 Installation 🔧
+
+```bash
+dotnet add package Neoxack.S3Kira.AspNetCore
+```
+
---
+++ src/s3kira.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace s3kira.AspNetCore;
+
+public static class ServiceCollectionExtensions
+{
+    private const string DefaultHttpClientName = "s3kira";
+        
+    public static IHttpClientBuilder AddS3Kira(this IServiceCollection services, S3KiraSettings settings)
+    {
+        var builder = services.AddHttpClient(DefaultHttpClientName)
+            .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
+            {
+                PooledConnectionLifetime = TimeSpan.FromMinutes(2)
+            })
+            .SetHandlerLifetime(Timeout.InfiniteTimeSpan);
+        
+        services.AddSingleton(settings);
+        services.AddSingleton(sp =>
+        {
+            var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+            var client = httpClientFactory.CreateClient(DefaultHttpClientName);
+            var s3KiraSettings = sp.GetRequiredService<S3KiraSettings>();
+            return new S3Kira(s3KiraSettings, client);
+        });
+
+        return builder;
+    }
+}
